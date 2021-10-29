3bf92d7445c1d11eacd7fbb5c3c79b2ac1b016d6
Erick012408 <85321107+Erick012408@users.noreply.github.com>
2021-10-29 11:35:35 -0500

    Add files via upload

---
+++ vistas/contenidos/es/web-index.php
@@ -0,0 +1,45 @@
+<div class="banner">
+    <div class="banner-body">
+        <h3 class="text-uppercase">Bienvenido a <?php echo COMPANY; ?></h3>
+        <p>Las mejores oportunidades de estudio, somo  <?php echo COMPANY; ?></p>
+    </div>
+</div>
+
+<div class="container-fluid container-web-page border-bottom bg-white">
+    <h3 class="text-center text-uppercase poppins-regular font-weight-bold">Nuestras oportunidades</h3>
+    <br>
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-4">
+            <p class="text-center"><i class="fas fa-shipping-fast fa-5x"></i></p>
+            <h5 class="text-center text-uppercase poppins-regular font-weight-bold">Integración</h5>
+            <p class="text-center">La escuela esta adapatada para que toda persona que caresca de habilidades fisicas pueda realitar todas su tareas sin nigun problema</p>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4">
+            <p class="text-center"><i class="fas fa-boxes fa-5x"></i></p>
+            <h5 class="text-center text-uppercase poppins-regular font-weight-bold">Maestros Capacitados</h5>
+            <p class="text-center">Con nuestros maestros capacitados, como para leer y hablar el lenguaje de señas, el braille </p>
+        </div>
+     
+    </div>
+</div>
+
+
+<div class="container container-web-page">
+    <div class="row justify-content-md-center">
+        <div class="col-12 col-md-6">
+            <figure class="full-box">
+                <img src="<?php echo SERVERURL; ?>vistas/assets/img/registration.png" alt="registration_<?php echo COMPANY; ?>" class="img-fluid">
+            </figure>
+        </div>
+        <div class="w-100"></div>
+        <div class="col-12 col-md-6">
+            <h3 class="text-center text-uppercase poppins-regular font-weight-bold">Crea tu cuenta</h3>
+            <p class="text-justify">
+                Crea tu cuenta para poder realizar la solicitud de ingreso
+            </p>
+            <p class="text-center">
+                <a href="<?php echo SERVERURL; ?>registration/" class="btn btn-primary" >Crear cuenta</a>
+            </p>
+        </div>
+    </div>
+</div>
